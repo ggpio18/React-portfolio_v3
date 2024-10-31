2a99e861b61c5c77c18919882e04de1bccd4aaec
ggpio18 <58065866+ggpio18@users.noreply.github.com>
2024-10-31 12:43:00 +0800

    +[testimonial boiler plate] ^[style &reponsive]

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -3,6 +3,7 @@ import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
 import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
 import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
+import Image from "next/image";
 
 const testimonials = [
   {
@@ -38,5 +39,21 @@ const testimonials = [
 ];
 
 export const TestimonialsSection = () => {
-  return <div>Testimonials Section</div>;
+  return ( 
+  <div>
+    <p>Happy Clients</p>
+    <h2>What Clients Say About me</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A vero similique, alias ipsa quibusdam architecto.</p>
+      <div>
+        {testimonials.map(testimonial => (
+          <div key={testimonial.name}>
+              <Image src={testimonial.avatar} alt={testimonial.name}/>
+              <div>{testimonial.name}</div>
+              <div>{testimonial.position}</div>
+              <p>{testimonial.text}</p>
+          </div>
+        ))}
+      </div>
+  </div>
+  );
 };
