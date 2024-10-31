7dc08c10dd6cd5649d9a823ae5599ed31bd09fd9
ggpio18 <58065866+ggpio18@users.noreply.github.com>
2024-10-31 12:06:28 +0800

    +[tape section]

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,34 @@
+import StarIcon from '@/assets/icons/star.svg'
+
+const words = [
+  "Performance",
+  "Accesible",
+  "Secure",
+  "Interactivve",
+  "Scalable",
+  "User Friendly",
+  "Responsive",
+  "Maintanable",
+  "Search Optimized",
+  "Usable",
+  "Reliable",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+  <div className='py-16 lg:py-24 overflow-x-clip'>
+    <div className='bg-gradient-to-r from-emerald-300 to-sky-400  -rotate-3 -mx-1'>
+      <div className='flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_10%,_black_90%,transparent)]'>
+        <div className='flex flex-none gap-4 py-4'>
+          {words.map(word => (
+            <div key={word} className='inline-flex gap-4 items-center'>
+              <span className='text-gray-900 uppercase font-semibold text-sm'>{word}</span>
+              <StarIcon className='size-6 text-gray-900 -rotate-12'/>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  </div>
+  );
 };
