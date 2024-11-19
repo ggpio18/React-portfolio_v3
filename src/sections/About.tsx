6c4b8ca2b812bf44e5f1ad5b48e7e1c2ba2d93f4
ggpio18 <58065866+ggpio18@users.noreply.github.com>
2024-11-20 00:20:27 +0800

    +[about section_|=bento_reponsive|cardhead|cards|testimonial]

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -91,37 +91,40 @@ const hobbies = [
 
 export const AboutSection = () => {
   return (
-    <div className="py-20">
+    <div className="py-20 lg:py-28">
       <div className="container">
         <SectionHeader
         eyebrow="About me"
         title="A glimpse Into my World"
         description="Learn more about who i am what i do and what inspires me"/>
           <div className="mt-20 flex flex-col gap-8">
-              <Card className="h-[320px]">
+            <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+              <Card className="h-[320px] col-span-2 lg:col-span-1">
                <CardHeader 
                title="My reads" 
                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit"
                />
-                <div className="w-40 mx-auto mt-8">
+                <div className="w-40 mx-auto mt-2 md:mt-0">
                   <Image src={bookImage} alt="Book cover"/>
                 </div>
               </Card>
 
-              <Card className="h-[320px] p-0">
+              <Card className="h-[320px]  col-span-3 lg:col-span-2">
                 <CardHeader 
                 title="My Toolbox" 
                 description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-                className="px-6 pt-6"
+                className=""
                 />
-                  <ToolboxItems items={toolboxItems} className="mt-6" itemsWrapperClassName=""/>
+                  <ToolboxItems items={toolboxItems} className="" itemsWrapperClassName=""/>
                   <ToolboxItems 
                   items={toolboxItems} 
                   className="mt-6 "
                   itemsWrapperClassName="-translate-x-1/2"/>
               </Card>
+            </div>
 
-              <Card className=" h-[320px] p-0 flex flex-col">
+            <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+              <Card className=" h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
                 <CardHeader 
                 title="Beyond the Code" 
                 description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
@@ -146,10 +149,17 @@ export const AboutSection = () => {
                     </div>
               </Card>
 
-              <Card>
-                <Image src={mapImage} alt="map"/>
-                <Image src={smileMemoji} alt="smilling memoji"/>
+              <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
+                <Image 
+                src={mapImage} 
+                alt="map" 
+                className="h-full w-full object-cover object-left-top"
+                />
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+                  <Image src={smileMemoji} alt="smilling memoji" className="size-20"/>
+                </div>
               </Card>
+            </div>
           </div>
       </div>
     </div>
