85a32b00c5520fa5540277492f047f10293dc5e9
ggpio18 <58065866+ggpio18@users.noreply.github.com>
2024-11-21 01:02:07 +0800

    +=[animate for_project|tape|testimonial sect]

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -56,11 +56,14 @@ export const ProjectsSection = () => {
       description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, saepe rerum?"/>
 
       <div className="mt-10 md:mt-20 flex flex-col gap-20">
-        {portfolioProjects.map(project => (
+        {portfolioProjects.map((project, projectIndex) => (
           <Card 
             key={project.title} 
-            className=" 
-            px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20">
+            className=" px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"
+            style={{
+              top: `calc(64px + ${projectIndex * 20}px`,
+            }}
+            >
          
           <div className="lg:grid lg:grid-cols-2 lg:gap-16">
             <div className="lg:pb-16">
