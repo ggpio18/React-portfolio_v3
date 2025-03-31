fcc10fca34c0aa449614c79ba291a649f94ae9af
ggpio18 <piolo18aranza@gmail.com>
2025-03-31 22:07:14 +0800

    +=[web analytics added]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import {Inter, Calistoga} from 'next/font/google'
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans"});
 const calistoga = Calistoga({
@@ -32,6 +33,7 @@ export default function RootLayout({
         "bg-gray-900 text-white antialiased font-sans"
         )}>
           {children}
+          <Analytics/>
           </body>
     </html>
   );
